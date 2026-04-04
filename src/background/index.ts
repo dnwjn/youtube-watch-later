43509cba3e6c07d3392c744b125c788e7b5885b7
Danique Wijnalda <hello@dnwjn.dev>
2026-04-04 20:14:49 +0200

    feat(whats-new): only open tab if the current version exists in the changelog

--- src/background/index.ts
+++ src/background/index.ts
@@ -1,5 +1,12 @@
+import packageJson from '@root/package.json'
+
+import { changelog, type ChangelogVersion } from '~changelog'
+
 chrome.runtime.onInstalled.addListener(({ reason }) => {
-  if (reason === 'update') {
+  if (
+    reason === 'update' &&
+    changelog.some((c: ChangelogVersion) => c.version === packageJson.version)
+  ) {
     chrome.tabs.create({ url: chrome.runtime.getURL('tabs/whats-new.html') })
   }
 })
