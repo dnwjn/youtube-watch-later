f91c733d2fa71d125275bcea3168302db4d15f9c
Danique Wijnalda <hello@dnwjn.dev>
2026-04-07 22:12:16 +0200

    fix(cookies): if present, pass store id when retrieving cookie

--- src/background/messages/visitor-cookie.ts
+++ src/background/messages/visitor-cookie.ts
@@ -1,9 +1,15 @@
 import type { PlasmoMessaging } from '@plasmohq/messaging'
 
 const handler: PlasmoMessaging.MessageHandler = async (req, res) => {
+  const cookieStoreId = (req.sender?.tab as any)?.cookieStoreId as string | undefined
+
   const sapisidCookie = await new Promise<string | null>((resolve, reject) => {
     chrome.cookies.get(
-      { url: 'https://www.youtube.com', name: 'SAPISID' },
+      {
+        url: 'https://www.youtube.com',
+        name: 'SAPISID',
+        ...(cookieStoreId ? { storeId: cookieStoreId } : {}),
+      },
       (cookie) => {
         if (cookie) {
           resolve(cookie.value)
